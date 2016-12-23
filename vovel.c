51e9cdc3a55f5b53edf5d0de792e5e0f9126d122
pondurai12 <pdurai31@gmail.com>
2016-12-23 11:44:21 +0530

    vowel.c
    check vovel or not

---
+++ vovel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+printf("enter char is vovel);
+}
+else
+{
+printf("enter char is not vovel");
+}
+getch();
+}
