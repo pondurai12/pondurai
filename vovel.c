bfc341ad1f80342441bd86d6e781c14a7016d951
pondurai12 <pdurai31@gmail.com>
2016-12-23 11:49:32 +0530

    vovel.c
    check vovel or not

--- vovel.c
+++ vovel.c
@@ -6,7 +6,7 @@ printf("enter the character");
 scanf("%c",&ch);
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 {
-printf("enter char is vovel);
+printf("enter char is vovel");
 }
 else
 {
